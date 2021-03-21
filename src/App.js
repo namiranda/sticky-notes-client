f8089c5676fd50b535f5bcae3d7e4f5b27b1a4c1
namiranda <49685356+namiranda@users.noreply.github.com>
2021-03-21 15:11:58 -0300

    landing para mobile lista

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import Landing from './Landing';
 export default () => {
   return (
     <div>
+      <div className="bg-yellow-300 h-14 ">
+        <h2 className="text-center text-3xl font-bold mont-alternates">
+          topiks
+        </h2>
+      </div>
       <Landing />
     </div>
   );
--- src/Landing.js
+++ src/Landing.js
@@ -2,16 +2,18 @@ import notes_landing from './img/notes_landing.png';
 
 export default () => {
   return (
-    <div>
-      <div>
+    <div className="h-full">
+      <div className="h-1/2">
         <img src={notes_landing} alt="" />
       </div>
-      <div>
-        <h2 className="text-lg text-indigo-900">Online Sticky Notes</h2>
-        <p className="text-indigo-900">
+      <div className="px-4">
+        <h1 className="text-5xl text-indigo-900 pb-2">Online Sticky Notes</h1>
+        <p className="text-xl text-indigo-900 py-4">
           Bring your team together for fun and effective remote meetings
         </p>
-        <button className="border-indigo-900">Get Started &gt;</button>
+        <button className="mb-4 px-1 text-xl text-indigo-900 uppercase border border-1 border-indigo-900">
+          Get Started &gt;
+        </button>
       </div>
     </div>
   );
