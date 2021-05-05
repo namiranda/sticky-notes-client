3914f15365b16be259c11d69a2a76d0e22caf3be
namiranda <49685356+namiranda@users.noreply.github.com>
2021-05-05 20:36:07 -0300

    ahora se muestran las notas antiguas y nuevas

--- src/Workspace.js
+++ src/Workspace.js
@@ -5,17 +5,19 @@ import socketIoClient from 'socket.io-client';
 //TODO: Sacar workspace id hardcodeado
 //Solucionar el resto de errores
 
-const socket = socketIoClient('http://localhost:3000', { autoConnect: false });
+const socket = socketIoClient('http://localhost:3000', {
+  autoConnect: false,
+});
 const ws_id = '60835b72b6f8c61c5885934a';
+
 const Note = ({ note }) => {
   return (
-    <div className="note">
-      <span> {new Date(note.date).toLocaleDateString()} </span>
-      <span> {note.content} </span>
+    <div className="bg-yellow-500">
+      {console.log('en note')}
+      <h2>{note}</h2>
     </div>
   );
 };
-
 const TextBox = () => {
   const [value, setValue] = useState('');
 
@@ -24,12 +26,10 @@ const TextBox = () => {
 
     if (!value) return;
 
-    socket.emit('message', ws_id, value);
-    console.log(value);
+    socket.emit('new note', ws_id, value);
 
     setValue('');
   };
-
   return (
     <form onSubmit={postNote}>
       <input
@@ -45,32 +45,31 @@ const TextBox = () => {
 
 const Workspace = () => {
   const [notes, setNotes] = useState([]);
-
   const addNote = (note) => {
-    setNotes((oldNotes) => [
-      ...oldNotes,
-      ...(Array.isArray(note) ? note.reverse() : [note]),
-    ]);
+    setNotes((oldNotes) => [...oldNotes, note]);
   };
 
   useEffect(() => {
-    socket.on('latest', (data) => {
-      // expect server to send us the latest messages
-      addNote(data);
+    socket.on('new note', (note) => {
+      addNote(note);
     });
-    socket.on('message', (msg) => {
-      addNote(msg);
+    socket.emit('old notes', ws_id);
+    socket.on('old notes', (data) => {
+      // expect server to send us the old notes
+      setNotes(Array.from(data));
     });
 
     socket.connect();
   }, []);
 
   return (
-    <div>
+    <div className="bg-red-400">
       <div>
-        {notes.map((note) => (
-          <Note msg={note} />
-        ))}
+        {Array.from(notes)
+          .reverse()
+          .map((note) => (
+            <Note key={note._id} note={note.content} />
+          ))}
       </div>
       <TextBox />
     </div>
